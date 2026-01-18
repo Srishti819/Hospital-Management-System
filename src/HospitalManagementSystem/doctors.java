b84356cde559f7314fb36d035067fa24566b253e
Srishti Yadav <srishtiyadav831102@gmail.com>
2026-01-18 19:25:56 +0530

    Initial project upload

--- src/HospitalManagementSystem/doctors.java
+++ src/HospitalManagementSystem/doctors.java
@@ -1,15 +1,41 @@
 package HospitalManagementSystem;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class doctors {
     Connection connection;
     Scanner scanner;
-    doctors(Connection connection, Scanner scanner){
+    public doctors(Connection connection, Scanner scanner){
         this.connection = connection;
         this.scanner=scanner;
     }
+
+    public void viewDoctors(){
+        String query = "SELECT * FROM doctors";
+        try{
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            ResultSet rs= preparedStatement.executeQuery();
+            System.out.println("doctor: ");
+            System.out.println("________________+___________________________+______________+________________");
+            System.out.println("| Doctors ID   | Name                      | Specialization                 ");
+            System.out.println("________________+___________________________+______________+________________");
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                String specialization = rs.getString("specialization");
+                System.out.printf("|%-3s|%-22s|%-10s|%-16s|\n" , id, name, specialization);
+                System.out.println("________________+___________________________+______________+________________");
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+
 
 
 
